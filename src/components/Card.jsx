c40050c895d4b0164ce7425d6d5af0b4e13c092f
Оргилсайхан Чулуунболор <23LP8480@imac888.local>
2024-01-12 15:42:09 -0800

    15pm

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,10 +1,76 @@
-// export default function Card({ name, count, setCount }) {
+import "../styles/card.css";
+export default function Card({ id, pName, pScore, increase, decrease }) {
+  return (
+    <div className="card">
+      <p>{pName}</p>
+      <div className="stepper">
+        <button onClick={() => decrease(id)}>-</button>
+        <p>{pScore}</p>
+        <button onClick={() => increase(id)}>+</button>
+      </div>
+    </div>
+  );
+}
+// export default function Card({ name, score, countUp, countDown }) {
 //   return (
-//     <div className="stepper">
+//     <div className="card">
 //       <p>{name}</p>
-//       <button onClick={() => setCount(Number(count) - 1)}>-</button>
-//       <p>{count}</p>
-//       <button onClick={() => setCount(Number(count) + 1)}>+</button>
+//       <div className="stepper">
+//         <button onClick={() => countDown(score)}> - </button>
+//         {score}
+//         <button onClick={() => countUp(score)}> + </button>
+//       </div>
 //     </div>
 //   );
 // }
+// const players = [
+//   {
+//     id: 1,
+//     playerName: "John Cena",
+//     playerScore: 10,
+//   },
+//   {
+//     id: 2,
+//     playerName: "John John",
+//     playerScore: 8,
+//   },
+//   {
+//     id: 3,
+//     playerName: "John Kena",
+//     playerScore: 5,
+//   },
+// ];
+
+// export const PlayerScore = () => {
+//   const [scores, setScores] = useState(players);
+
+//   const increase = (index) => {
+//     const updatedScores = [...scores];
+//     updatedScores[index].playerScore += 1;
+//     setScores(updatedScores);
+//   };
+
+//   const decrease = (index) => {
+//     const updatedScores = [...scores];
+//     updatedScores[index].playerScore -= 1;
+//     setScores(updatedScores);
+//   };
+
+//   // const increase = (index) => {
+//   //   setScores((players[index].playerScore = +1));
+//   //   // result number, string
+//   // };
+//   return (
+//     <div>
+//       {scores.map((player, index) => (
+//         <div key={index}>
+//           <button onClick={() => increase(index)}>+</button>
+//           <span>
+//             {player.playerName}: {player.playerScore}
+//           </span>
+//           <button onClick={() => decrease(index)}>-</button>
+//         </div>
+//       ))}
+//     </div>
+//   );
+// };
--- src/components/CardContainer.jsx
+++ src/components/CardContainer.jsx
@@ -1,43 +1,163 @@
 import { useState } from "react";
 import "../styles/cardContainer.css";
-// import Card from "./Card";
-export default function CardContainer() {
-  const [count1, setCount1] = useState(1);
-  const [count2, setCount2] = useState(1);
-  const [count3, setCount3] = useState(1);
-  let names = ["Orgil", "Khuslen", "Ganbaa"];
-  function resetBtn() {
-    setCount1(1);
-    setCount2(1);
-    setCount3(1);
-  }
+import Card from "./Card";
+const players = [
+  {
+    id: 1,
+    pName: "Orgil",
+    pScore: 0,
+  },
+  {
+    id: 2,
+    pName: "Bilguun",
+    pScore: 0,
+  },
+  {
+    id: 3,
+    pName: "Enkhee",
+    pScore: 0,
+  },
+];
+const CardContainer = () => {
+  const [scores, setScores] = useState(players);
+  const increase = (id) => {
+    let updateScores = scores.map((score) => {
+      if (score.id == id) {
+        score.pScore++;
+      }
+      return score;
+    });
+    updateScores.sort((b, a) => a.pScore - b.pScore);
+    setScores(updateScores);
+    console.log(updateScores);
+  };
+  const decrease = (id) => {
+    let updateScores = scores.map((score) => {
+      if (score.id == id) {
+        score.pScore--;
+      }
+      return score;
+    });
+    updateScores.sort((b, a) => a.pScore - b.pScore);
+    setScores(updateScores);
+  };
+  // const increase = (index) => {
+  //   const updateScores = [...scores];
+  //   updateScores[index].pScore += 1;
+  //   setScores(updateScores);
+  // };
+  // const decrease = (index) => {
+  //   const updateScores = [...scores];
+  //   updateScores[index].pScore -= 1;
+  //   setScores(updateScores);
+  // };
+
   return (
     <div className="cardContainer">
-      <div className="card">
-        <p>John Cena</p>
-        <div className="stepper">
-          <button onClick={() => setCount1(count1 - 1)}>-</button>
-          {count1}
-          <button onClick={() => setCount1(count1 + 1)}>+</button>
-        </div>
-      </div>
-      <div className="card">
-        <p>John Cena</p>
-        <div className="stepper">
-          <button onClick={() => setCount2(count2 - 1)}>-</button>
-          {count2}
-          <button onClick={() => setCount2(count2 + 1)}>+</button>
-        </div>
-      </div>
-      <div className="card">
-        <p>John Cena</p>
-        <div className="stepper">
-          <button onClick={() => setCount3(count3 - 1)}>-</button>
-          {count3}
-          <button onClick={() => setCount3(count3 + 1)}>+</button>
-        </div>
-      </div>
-      <button onClick={resetBtn}>ResetBtn</button>
+      {scores.map((player, index) => {
+        return (
+          <Card
+            pName={player.pName}
+            pScore={player.pScore}
+            id={player.id}
+            key={index}
+            increase={increase}
+            decrease={decrease}
+          />
+        );
+      })}
+      <button>Reset</button>
     </div>
   );
-}
+};
+export default CardContainer;
+//   const [count1, setCount1] = useState(1);
+//   const [count2, setCount2] = useState(1);
+//   const [count3, setCount3] = useState(1);
+//   const data = [
+//     {
+//       name: "Orgil",
+//       score: count1,
+//       id: 1,
+//     },
+//     {
+//       name: "Enkhee",
+//       score: count2,
+//       id: 2,
+//     },
+//     {
+//       name: "Tsendee",
+//       score: count3,
+//       id: 3,
+//     },
+//   ];
+//   data.sort((a, b) => b.score - a.score);
+
+//   const [scores, setScores] = useState(data);
+
+//   function resetBtn() {
+//     setCount1(1);
+//     setCount2(1);
+//     setCount3(1);
+//   }
+//   return (
+//     <div className="cardContainer">
+//       {data.map((e) => {
+//         return (
+//           <Card
+//             name={e.name}
+//             score={e.score}
+//             countUp={() => {
+//               // if (e.id == 1) {
+//               //   return setCount1(count1 + 1);
+//               // }
+//               // if (e.id == 2) {
+//               //   return setCount2(count2 + 1);
+//               // }
+//               // if (e.id == 3) {
+//               //   return setCount3(count3 + 1);
+//               // }
+//               e.id == 1
+//                 ? setCount1(count1 + 1)
+//                 : e.id == 2
+//                 ? setCount2(count2 + 1)
+//                 : setCount3(count3 + 1);
+//             }}
+//             countDown={() => {
+//               e.id == 1
+//                 ? setCount1(count1 - 1)
+//                 : e.id == 2
+//                 ? setCount2(count2 - 1)
+//                 : setCount3(count3 - 1);
+//             }}
+//           />
+//         );
+//       })}
+//       <div className="card">
+//         <p>{data[0].name}</p>
+//         <div className="stepper">
+//           <button onClick={() => setCount1(count1 - 1)}>-</button>
+//           {count1}
+//           <button onClick={() => setCount1(count1 + 1)}>+</button>
+//         </div>
+//       </div>
+//       <div className="card">
+//         <p>{data[1].name}</p>
+//         <div className="stepper">
+//           <button onClick={() => setCount2(count2 - 1)}>-</button>
+//           {count2}
+//           <button onClick={() => setCount2(count2 + 1)}>+</button>
+//         </div>
+//       </div>
+//       <div className="card">
+//         <p>{data[2].name}</p>
+//         <div className="stepper">
+//           <button onClick={() => setCount3(count3 - 1)}>-</button>
+//           {count3}
+//           <button onClick={() => setCount3(count3 + 1)}>+</button>
+//         </div>
+//       </div>
+//       <button onClick={resetBtn}>ResetBtn</button>
+//     </div>
+//   );
+// }
